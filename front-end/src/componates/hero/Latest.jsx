9563005d1a6811907bc0b27b1f046613caf402e4
abderrahmane bencheikh <168656302+bencheikh-mohamed-abderrhmane@users.noreply.github.com>
2024-08-19 15:07:46 +0100

    Update Latest.jsx

--- front-end/src/componates/hero/Latest.jsx
+++ front-end/src/componates/hero/Latest.jsx
@@ -6,7 +6,7 @@ function Latest() {
     const [new_collection, setNew_collection] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/newcollections')
+        fetch('https://game-heaven-front-end.onrender.com/newcollections')
             .then((response) => response.json())
             .then((data) => setNew_collection(data));
     }, []);
