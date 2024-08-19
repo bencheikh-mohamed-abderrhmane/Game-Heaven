e245d41c86565deb873649d57ebf1667ccec119d
abderrahmane bencheikh <168656302+bencheikh-mohamed-abderrhmane@users.noreply.github.com>
2024-08-19 11:40:53 +0100

    Update Newcollections.jsx

--- front-end/src/componates/newcollections/Newcollections.jsx
+++ front-end/src/componates/newcollections/Newcollections.jsx
@@ -6,7 +6,7 @@ function Newcollections(props) {
 
      const [new_collection,setNew_collection] = useState([])
      useEffect(()=>{
-        fetch('http://localhost:4000/newcollections')
+        fetch('https://game-heaven-back-end.onrender.com/newcollections')
         .then((response)=>response.json())
         .then((data)=>setNew_collection(data))
 
