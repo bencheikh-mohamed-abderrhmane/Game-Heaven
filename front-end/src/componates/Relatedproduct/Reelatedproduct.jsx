deaf5b74e2611e81752730905fee88552db8e3e1
abderrahmane bencheikh <168656302+bencheikh-mohamed-abderrhmane@users.noreply.github.com>
2024-08-20 16:34:40 +0100

    Update Reelatedproduct.jsx

--- front-end/src/componates/Relatedproduct/Reelatedproduct.jsx
+++ front-end/src/componates/Relatedproduct/Reelatedproduct.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import './relatedproduct.css'
-import data_product from '../assets/data';
+import React, { useEffect, useState } from 'react';
+import './relatedproduct.css';
 import Item from '../items/Item';
 
-function Reelatedproduct(props) {
+function Reelatedproduct() {
+    const [relatedProducts, setRelatedProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://game-heaven-back-end.onrender.com/newcollections')
+            .then((response) => response.json())
+            .then((data) => {
+                // Assuming 'data' is an array of products
+                const limitedProducts = data.slice(2,7 ); // Limit to 5 products
+                setRelatedProducts(limitedProducts);
+            })
+            .catch((error) => console.error('Error fetching related products:', error));
+    }, []);
+
     return (
         <div className='relatedproduct'>
             <h1>Related Products</h1>
             <hr />
             <div className="relatedproducts-item">
-                {data_product.map((item,i)=>{
-                    return <Item  key={i} id={item.id} name={item.name} image={item.Image} new_price={item.new_price} old_price={item.old_price}/>
-
-                })}
+                {relatedProducts.map((item, i) => (
+                    <Item 
+                        key={i} 
+                        id={item.id} 
+                        name={item.name} 
+                        image={item.image} 
+                        new_price={item.new_price} 
+                        old_price={item.old_price}
+                    />
+                ))}
             </div>
-            
         </div>
     );
 }
